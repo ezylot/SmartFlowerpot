20ca7c7bcf44430c78e5030f86765a8b2a893a2e
LukasStallinger <l.stallinger@gmx.t>
2019-06-19 10:59:55 +0000

    client

--- motion_detection.py
+++ motion_detection.py
@@ -2,6 +2,7 @@
 
 import picamera
 import picamera.array
+import subprocess
 import time
 import paho.mqtt.publish as publish
 from sense_hat import SenseHat
@@ -11,14 +12,14 @@ MQTT_SERVER = "192.168.43.136"
 MQTT_PATH = "SmartFlowerpot"
 
 threshold = 10    # How Much pixel changes
-sensitivity = 100 # How many pixels change
+sensitivity = 2000 # How many pixels change
 
 sense = SenseHat()
 sense.clear()
 
 def takeMotionImage(width, height):
     with picamera.PiCamera() as camera:
-        time.sleep(1)
+        time.sleep(0.2)
         camera.resolution = (width, height)
         with picamera.array.PiRGBArray(camera) as stream:
             camera.exposure_mode = 'auto'
@@ -50,16 +51,18 @@ def scanMotion(width, height):
 def motionDetection():
     print("Scanning for Motion threshold=%i sensitivity=%i..."  % (threshold, sensitivity))
     while True:
-        try:
+       # try:
           if scanMotion(224, 160):
               temp = sense.get_temperature()
-              print ("Motion detected --> Temperatur is: " + str(temp))
+              cpu_temp = subprocess.check_output("cat /sys/class/thermal/thermal_zone0/temp", shell=True)
+              realtemp = temp - (((float(cpu_temp)/1000) - temp)/1.2)
+              print ("Motion detected --> Temperatur is: " + str(realtemp))
               try:
-                publish.single(MQTT_PATH, str(temp), hostname=MQTT_SERVER)
+                publish.single(MQTT_PATH, str(realtemp), hostname=MQTT_SERVER)
               except:
                 print("server unreachable")
-        except:
-          print("something went wrong")
+      #  except:
+      #    print("something went wrong")
 
 if __name__ == '__main__':
     try:
